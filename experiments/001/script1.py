81ab29b9b1995cf2f4f450c8a4061b0050ee6887
sazzadi-r14 <sazzadi.cc9@gmail.com>
2023-09-06 15:03:39 +0600

    CLustering Function Ready

--- experiments/001/script1.py
+++ experiments/001/script1.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import json
-import ast
+from global_utils import *
 
 with open('../apipayload.json', 'r') as json_file:
     data = json.load(json_file)
+    
+    
+with open('../groundtruth.csv', 'r') as csv_file:
+    gt = pd.read_csv(csv_file)
 
-var = data[0]['Azure']
-print(var["readResult"]['pages'][0]['lines'])
+var = data[4]['Azure']
+
+api_data = var["readResult"]['pages'][0]['lines']
+
+line_info = line_data(api_data)
+    
+    
+line_info = indent(line_info)
+
+print(gt['Ground Truth'][4])
+print(line_info)
